6f4ba3cb3f5024dcfba6758481ebc71c459b2b42
Justin Etzine <justetz@gmail.com>
2016-12-17 10:14:42 -0500

    Implemented a updates page mockup

---
+++ mockups/updates.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html>
+<head>
+  <title>Updates | RPI TV</title>
+  <?php echo file_get_contents('partials/head.html'); ?>
+</head>
+<body class="grey darken-4">
+  <?php echo file_get_contents('partials/nav.html'); ?>
+  <div class="container section">
+    <div class="row">
+      <div class="col s12 m6 l7">
+        <h4 class="white-text">News Updates</h4>
+        <div class="row">
+          <div class="col s12">
+            <div class="card news-update">
+              <div class="card-image">
+                <img src="http://rpitv.org/sys_i/original/911.jpg">
+                <span class="card-title flow-text">RPI TV investigating alleged sign policy violations</span>
+              </div>
+              <div class="card-content">
+                <p class="small grey-text lighten-2">April 09, 2016</p>
+                <p class="truncate">This morning, RPI TV learned that students postering with the Save the Union movement found RPI staff removing their posters in contradiction of the long-standing Institute sign policy.</p>
+              </div>
+              <div class="card-action right-align">
+                <a href="#">View Footage</a>
+                <a href="#">Read more</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col s12 m6 l5">
+        <h5 class="white-text">Upcoming Club Events</h5>
+        <ul class="collection">
+          <li class="collection-item avatar">
+            <i class="material-icons circle">folder</i>
+            <span class="title">Title</span>
+            <p>First Line <br>
+               Second Line
+            </p>
+          </li>
+          <li class="collection-item avatar">
+            <i class="material-icons circle">folder</i>
+            <span class="title">Title</span>
+            <p>First Line <br>
+               Second Line
+            </p>
+          </li>
+          <li class="collection-item avatar">
+            <i class="material-icons circle green">insert_chart</i>
+            <span class="title">Title</span>
+            <p>First Line <br>
+               Second Line
+            </p>
+          </li>
+          <li class="collection-item avatar">
+            <i class="material-icons circle red">play_arrow</i>
+            <span class="title">Title</span>
+            <p>First Line <br>
+               Second Line
+            </p>
+          </li>
+        </ul>
+        <div class="right-align">
+          <a class="waves-effect waves-light btn">View More</a>
+        </div>
+        <h5 class="white-text">Club Documents</h5>
+        <div class="collection">
+          <a class="collection-item avatar">
+            <i class="material-icons circle">folder</i>
+            <span class="title">Business Meeting Minutes</span>
+            <p>Tuesday, November 29, 2016</p>
+          </a>
+          <a class="collection-item avatar">
+            <i class="material-icons circle">folder</i>
+            <span class="title">Title</span>
+            <p>First Line <br>
+               Second Line
+            </p>
+          </a>
+          <a class="collection-item avatar">
+            <i class="material-icons circle green">insert_chart</i>
+            <span class="title">Title</span>
+            <p>First Line <br>
+               Second Line
+            </p>
+          </a>
+          <a class="collection-item avatar">
+            <i class="material-icons circle red">play_arrow</i>
+            <span class="title">Title</span>
+            <p>First Line <br>
+               Second Line
+            </p>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php echo file_get_contents('partials/footer.html'); ?>
+  <?php echo file_get_contents('partials/bottom_scripts.html'); ?>
+</body>
+</html>
