ea8a5ddb0d884c615916f9ed179d8f53c5634d21
Justin Etzine <justetz@gmail.com>
2016-12-17 10:46:09 -0500

    Updates page tweaks

--- mockups/updates.php
+++ mockups/updates.php
@@ -27,6 +27,64 @@
               </div>
             </div>
           </div>
+          <div class="col s12">
+            <div class="card news-update">
+              <div class="card-image">
+                <img src="http://rpitv.org/sys_i/original/905.PNG">
+                <span class="card-title flow-text">Censorship on campus: RPI TV denied right to record campus events</span>
+              </div>
+              <div class="card-content">
+                <p class="small grey-text lighten-2">March 29, 2016</p>
+                <p class="truncate">In the past, RPI TV has covered controversial situations like present times. We have been allowed unprecedented access to campus events, and we use this access to keep students informed.</p>
+              </div>
+              <div class="card-action right-align">
+                <a href="#">Read more</a>
+              </div>
+            </div>
+          </div>
+          <div class="col s12">
+            <div class="card news-update">
+              <div class="card-image">
+                <img src="http://rpitv.org/sys_i/original/820.jpg">
+                <span class="card-title flow-text">RPI Hockey playoffs will not be streamed live on RPI TV</span>
+              </div>
+              <div class="card-content">
+                <p class="small grey-text lighten-2">March 03, 2016</p>
+                <p class="truncate">For over a decade, RPI TV has brought top-quality video production to Rensselaer Polytechnic Institute and the world through free internet streaming. Staffed by student-volunteers, RPI TV’s mission has been to provide the best possible video coverage of campus events to the global Rensselaer community and others wishing to stay up-to-date on RPI events.</p>
+              </div>
+              <div class="card-action right-align">
+                <a href="#">Visit ECAC Hockey Website</a>
+                <a href="#">Read more</a>
+              </div>
+            </div>
+          </div>
+          <div class="col s12">
+            <div class="card news-update">
+              <div class="card-image">
+                <img src="http://rpitv.org/sys_i/original/846.png">
+                <span class="card-title flow-text">A Strong Rensselaer Union is a Student-run Union</span>
+              </div>
+              <div class="card-content">
+                <p class="small grey-text lighten-2">April 16, 2015</p>
+                <p class="truncate">During the GM/PU debates yesterday evening the following question was asked in reference to the Rensselaer Union’s financial state: "You've said that you want to cut down administrative costs. Can you elaborate on what exactly you'd like to cut?"</p>
+              </div>
+              <div class="card-action right-align">
+                <a href="#">Watch Debate</a>
+                <a href="#">Read more</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row center-align">
+          <ul class="pagination">
+            <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+            <li class="active"><a href="#!">1</a></li>
+            <li class="waves-effect"><a href="#!">2</a></li>
+            <li class="waves-effect"><a href="#!">3</a></li>
+            <li class="waves-effect"><a href="#!">4</a></li>
+            <li class="waves-effect"><a href="#!">5</a></li>
+            <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+          </ul>
         </div>
       </div>
       <div class="col s12 m6 l5">
@@ -62,36 +120,32 @@
           </li>
         </ul>
         <div class="right-align">
-          <a class="waves-effect waves-light btn">View More</a>
+          <a class="waves-effect waves-light btn red darken-4">View More</a>
         </div>
         <h5 class="white-text">Club Documents</h5>
-        <div class="collection">
+        <div class="document-collection">
+          <a class="collection-item">
+            <span class="title">Business Meeting Minutes</span>
+            <p>Tuesday, November 29, 2016</p>
+          </a>
           <a class="collection-item avatar">
+            <i class="material-icons circle">folder</i>
+            <span class="title">Business Meeting Minutes</span>
+            <p>Tuesday, November 29, 2016</p>
+          </a>
+          <a class="collection-item avatar document">
             <i class="material-icons circle">folder</i>
             <span class="title">Business Meeting Minutes</span>
             <p>Tuesday, November 29, 2016</p>
           </a>
           <a class="collection-item avatar">
             <i class="material-icons circle">folder</i>
-            <span class="title">Title</span>
-            <p>First Line <br>
-               Second Line
-            </p>
+            <span class="title">Business Meeting Minutes</span>
+            <p>Tuesday, November 29, 2016</p>
           </a>
-          <a class="collection-item avatar">
-            <i class="material-icons circle green">insert_chart</i>
-            <span class="title">Title</span>
-            <p>First Line <br>
-               Second Line
-            </p>
-          </a>
-          <a class="collection-item avatar">
-            <i class="material-icons circle red">play_arrow</i>
-            <span class="title">Title</span>
-            <p>First Line <br>
-               Second Line
-            </p>
-          </a>
+        </div>
+        <div class="right-align">
+          <a class="waves-effect waves-light btn red darken-4">View More</a>
         </div>
       </div>
     </div>
