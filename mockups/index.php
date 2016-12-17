ebc6de6bd688aa4148e30f0fae17c6f2f9465b94
ddbruce <daniel@danieldbruce.com>
2016-12-16 21:51:57 -0500

    work on things

--- mockups/index.php
+++ mockups/index.php
@@ -10,10 +10,10 @@
 			<a href="#" class="brand-logo"><img src="resources/img/rpitv_logo.min.svg" alt="rpitv_logo" class=navbar-logo></a>
 			<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
 			<ul id="nav-mobile" class="right hide-on-med-and-down">
-				<li><a class="waves-effect waves-light" href="productions">Productions</a></li>
-				<li><a class="waves-effect waves-light" href="news-updates">News Updates</a></li>
-				<li><a class="waves-effect waves-light" href="services">Services</a></li>
-				<li><a class="waves-effect waves-light" href="about">About</a></li>
+				<li><a class="waves-effect waves-light" href="productions.php">Productions</a></li>
+				<li><a class="waves-effect waves-light" href="news-updates.php">News Updates</a></li>
+				<li><a class="waves-effect waves-light" href="services.php">Services</a></li>
+				<li><a class="waves-effect waves-light" href="about.php">About</a></li>
 			</ul>
 			<ul class="side-nav" id="mobile-demo">
 				<li><a href="productions">Productions</a></li>
@@ -25,6 +25,8 @@
 	</nav>
 	<div class="parallax-container">
 		<div class="section no-pad-bot">
+			<br>
+			<br>
 			<div class="row parallax-content center">
 				<h1 class="header white-text">Big Red Freakout!</h1>
 				<h5 class="header white-text">The 40th Annual - RPI vs. Quinnipiac</h5>
@@ -39,7 +41,7 @@
 	<div class="container section">
 		<div class="row">
 			<div class="col s12">
-				<h4 class="white-text">Featured productions</h4>
+				<h4 class="white-text">Recent productions</h4>
 			</div>
 		</div>
 		<div class="row">
@@ -78,7 +80,21 @@
 						<span class="card-title">Women's Hockey vs. Quinnipiac</span>
 					</div>
 					<div class="card-content">
-						<p class=truncate>The ACHA-league RPI Engineers take on the UMass Amherst something-or-others at the Houston Field House.</p>
+						<p class=truncate>The Engineers take on the Bobcats in this Friday night rouse.</p>
+					</div>
+					<div class="card-action">
+						<a class="on-demand" href="/productions/1190-acha-hockey-vs-umass-amherst">Watch on-demand</a>
+					</div>
+				</div>
+			</div>
+			<div class="col s12 m6">
+				<div class="card featured-production">
+					<div class="card-image">
+						<img src="http://rpitv.org/sys_i/original/1010.png">
+						<span class="card-title">Jazz at Rensselaer: Big Swells and Jingle Bells and a Longer Title</span>
+					</div>
+					<div class="card-content">
+						<p class=truncate>The Engineers take on the Bobcats in this Friday night rouse.</p>
 					</div>
 					<div class="card-action">
 						<a class="on-demand" href="/productions/1190-acha-hockey-vs-umass-amherst">Watch on-demand</a>
