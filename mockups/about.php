b70310ed51f749b0bd8fbfbe174961a25d5d0c32
Justin Etzine <justetz@gmail.com>
2016-12-17 14:09:23 -0500

    About page started

---
+++ mockups/about.php
@@ -0,0 +1,155 @@
+<!doctype html>
+<html>
+<head>
+  <title>Updates | RPI TV</title>
+  <?php echo file_get_contents('partials/head.html'); ?>
+</head>
+<body class="grey darken-4">
+  <?php echo file_get_contents('partials/nav.html'); ?>
+  <div class="container section white-text">
+    <div class="row">
+      <div class="col s12">
+        <h3>About RPI TV</h3>
+        <h4>About</h4>
+        <p>RPI TV is the student-run television station for Rensselaer Polytechnic Institute.  We are separate from the RPI campus cable TV service, which is managed by <a href="http://mms.rpi.edu/">MultiMedia Services</a>.  We provide TV production and web streaming services to campus organizations and film lots of events around campus, including hockey games, football games, news programs, cultural performances, and the GM Week Debates.</p>
+        <br>
+        <h4>How To Join</h4>
+        <p>We have a lot of fun, and we think you will too. Why not join us and find out? We meet every Tuesday at 9pm in Union 3202.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col s12">
+        <h4>Officers</h4>
+      </div>
+      <div class="col s12 m6 l4">
+        <div class="card">
+          <div class="card-image officer-image">
+            <img src="http://rpitv.org/sys_i/original/994.jpg">
+          </div>
+          <div class="card-content">
+            <span class="card-title grey-text text-darken-4">Michael Cuozzo</span>
+            <p class="grey-text text-darken-4">President</p>
+          </div>
+        </div>
+      </div>
+      <div class="col s12 m6 l4">
+        <div class="card">
+          <div class="card-image officer-image">
+            <img src="http://rpitv.org/sys_i/original/881.jpg">
+          </div>
+          <div class="card-content">
+            <span class="card-title grey-text text-darken-4">Richard Lin</span>
+            <p class="grey-text text-darken-4">Vice President</p>
+          </div>
+        </div>
+      </div>
+      <div class="col s12 m6 l4">
+        <div class="card">
+          <div class="card-image officer-image">
+            <img src="http://rpitv.org/sys_i/original/866.png">
+          </div>
+          <div class="card-content">
+            <span class="card-title grey-text text-darken-4">Jason Lee</span>
+            <p class="grey-text text-darken-4">Productions Manager</p>
+          </div>
+        </div>
+      </div>
+      <div class="col s12 m6 l4">
+        <div class="card">
+          <div class="card-image officer-image">
+            <img src="http://rpitv.org/sys_i/original/991.jpg">
+          </div>
+          <div class="card-content">
+            <span class="card-title grey-text text-darken-4">Dan Bruce</span>
+            <p class="grey-text text-darken-4">Facilities and Equipment Manager</p>
+          </div>
+        </div>
+      </div>
+      <div class="col s12 m6 l4">
+        <div class="card">
+          <div class="card-image officer-image">
+            <img src="http://rpitv.org/sys_i/original/884.jpg">
+          </div>
+          <div class="card-content">
+            <span class="card-title grey-text text-darken-4">Justin Etzine</span>
+            <p class="grey-text text-darken-4">Business Manager</p>
+          </div>
+        </div>
+      </div>
+      <div class="col s12 m6 l4">
+        <div class="card">
+          <div class="card-image officer-image">
+            <img src="http://rpitv.org/sys_i/original/976.jpg">
+          </div>
+          <div class="card-content">
+            <span class="card-title grey-text text-darken-4">Roshaun Brady</span>
+            <p class="grey-text text-darken-4">Secretary</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col s12">
+        <h4>Members</h4>
+      </div>
+      <div class="col s12 m4 l3">
+        <div class="card">
+          <div class="card-image member-image">
+            <img src="http://rpitv.org/sys_i/original/963.jpg">
+          </div>
+          <div class="card-content">
+            <span class="card-title grey-text text-darken-4">Noah Akinloba</span>
+            <p class="grey-text text-darken-4">Class of 2020</p>
+          </div>
+        </div>
+      </div>
+      <div class="col s12 m4 l3">
+        <div class="card">
+          <div class="card-image member-image">
+            <img src="http://rpitv.org/sys_i/original/171.jpg">
+          </div>
+          <div class="card-content">
+            <span class="card-title grey-text text-darken-4">Andrew Armenia</span>
+            <p class="grey-text text-darken-4">Class of 2010</p>
+          </div>
+        </div>
+      </div>
+      <div class="col s12 m4 l3">
+        <div class="card">
+          <div class="card-image member-image">
+            <img src="http://rpitv.org/sys_i/original/979.jpg">
+          </div>
+          <div class="card-content">
+            <span class="card-title grey-text text-darken-4">Erica Braunschweig</span>
+            <p class="grey-text text-darken-4">Class of 2017</p>
+          </div>
+        </div>
+      </div>
+      <div class="col s12 m4 l3">
+        <div class="card">
+          <div class="card-image member-image">
+            <img src="http://rpitv.org/sys_i/original/980.jpg">
+          </div>
+          <div class="card-content">
+            <span class="card-title grey-text text-darken-4">Anthony Chen</span>
+            <p class="grey-text text-darken-4">Class of 2020</p>
+          </div>
+        </div>
+      </div>
+      <div class="col s12 m4 l3">
+        <div class="card">
+          <div class="card-image member-image">
+            <img src="http://rpitv.org/sys_i/original/984.jpg">
+          </div>
+          <div class="card-content">
+            <span class="card-title grey-text text-darken-4">Noa Davis</span>
+            <p class="grey-text text-darken-4">Class of 2019</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php echo file_get_contents('partials/footer.html'); ?>
+  <?php echo file_get_contents('partials/bottom_scripts.html'); ?>
+</body>
+</html>
