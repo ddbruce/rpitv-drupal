b42cce27bfdef126a901ac857d381ed82d9e7e93
Justin Etzine <justetz@gmail.com>
2016-12-19 18:38:30 -0500

    Begun production detail page

---
+++ mockups/production_detail.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+  <title>Productions | RPI TV</title>
+  <?php echo file_get_contents('partials/head.html'); ?>
+</head>
+<body class="grey darken-4">
+  <?php echo file_get_contents('partials/nav.html'); ?>
+  <div class="container section">
+    <div class="row">
+      <div class="col s12">
+        <div class="video-container video-card">
+          <iframe src="https://www.youtube.com/embed/hJQYY0_sYXI" frameborder="0" allowfullscreen></iframe>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col s12">
+        <div class="card-panel">
+          <h5>ACHA Hockey vs. UMass Amherst</h5>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php echo file_get_contents('partials/footer.html'); ?>
+  <?php echo file_get_contents('partials/bottom_scripts.html'); ?>
+</body>
+</html>
